0c8219ecff995a790594b941e949d57bbe9928bb
wantao666 <895484122@qq.com>
2020-04-24 13:43:52 +0800

    2020/4/24 13:43

--- pages/register.vue
+++ pages/register.vue
@@ -19,24 +19,24 @@
       <el-main>
         <div style="width:500px;margin:0 auto;">
           <el-form :model="registerForm" :rules="rules" label-width="90px">
-            <el-form-item label="昵称" :prop="registerForm.nickname">
+            <el-form-item label="昵称" prop="nickname">
               <el-input v-model="registerForm.nickname"></el-input>
             </el-form-item>
-            <el-form-item label="邮箱" :prop="registerForm.email">
+            <el-form-item label="邮箱" prop="email">
               <el-input v-model="registerForm.email"></el-input>
               <el-button type="info" style="margin-top:10px;">获取邮箱验证码</el-button>
               <span>{{statusMsg}}</span>
             </el-form-item>
-            <el-form-item label="邮箱验证码" :prop="registerForm.emailCode">
+            <el-form-item label="邮箱验证码" prop="emailCode">
               <el-input v-model="registerForm.emailCode"></el-input>
             </el-form-item>
-            <el-form-item label="创建密码" :prop="registerForm.password">
+            <el-form-item label="创建密码" prop="password">
               <el-input v-model="registerForm.password"></el-input>
             </el-form-item>
             <el-form-item>
               <password-stronger v-model="registerForm.password" style="padding-top: 10px;"></password-stronger>
             </el-form-item>
-            <el-form-item label="确认密码" :prop="registerForm.password1">
+            <el-form-item label="确认密码" prop="password1">
               <el-input v-model="registerForm.password1"></el-input>
             </el-form-item>
             <el-form-item>
@@ -88,10 +88,16 @@ export default {
             trigger: "blur"
           }
         ],
+        emailCode: [
+          {
+            required: true,
+            type: "string",
+            trigger: "blur"
+          }
+        ],
         password: [
           {
             required: true,
-            type: "password",
             message: "创建密码",
             trigger: "blur"
           }
@@ -100,14 +106,15 @@ export default {
           { required: true, message: "确认密码", trigger: "blur" },
           {
             validator: (rule, value, cb) => {
-              if (value == "") {
+              if (value === "") {
                 cb(new Error("密码不能为空"));
-              } else if (value != this.registerForm.password) {
+              } else if (value !== this.registerForm.password) {
                 cb(new Error("两次密码输入不一致"));
               } else {
                 cb();
               }
-            }
+            },
+            trigger: "blur"
           }
         ]
       }
